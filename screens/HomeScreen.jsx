3940397c9e68e6c0fe97262dbad0bf418f961ee5
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2022-10-20 18:39:36 +0300

    Added GooglePlacesAutocomplete input

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Image, View } from 'react-native';
 import NavOptions from '../components/NavOptions';
+import AutocompleteInput from '../components/AutocompleteInput';
 import globalStyles from '../globalStyles';
 import tw from 'tailwind-react-native-classnames';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-// import { GOOGLE_MAPS_API_KEY } from 'react-native-dotenv';
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={[globalStyles.AndroidSafeArea, tw`bg-white h-full`]}>
             <View style={tw`p-5`}>
                 <Image
-                    style={{
-                        width: 100,
-                        height: 100,
-                        resizeMode: 'contain',
-                    }}
-                    source={{
-                        uri: 'https://links.papareact.com/gzs',
-                    }}
+                    style={{ width: 100, height: 100, resizeMode: 'contain' }}
+                    source={{ uri: 'https://links.papareact.com/gzs' }}
                 />
+                <AutocompleteInput />
                 <NavOptions />
             </View>
         </SafeAreaView>
